70274747839e283c3dfc15205a7fb7e2fe7512bb
dangyulee <dg200101@gmail.com>
2025-07-20 14:06:05 +0900

    cors

--- movie_review/movie_review/settings.py
+++ movie_review/movie_review/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -209,6 +210,14 @@ SIMPLE_JWT = {
     'TOKEN_TYPE_CLAIM': 'token_type',
     'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
 }
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+    "https://kikoky.shop",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 try:
     from .local_settings import *
