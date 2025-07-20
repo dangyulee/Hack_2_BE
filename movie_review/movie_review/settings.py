261240ed7bb66ceffb7bd6418f8a8d0d7d595cec
dangyulee <dg200101@gmail.com>
2025-07-20 14:35:02 +0900

    1234

--- movie_review/movie_review/settings.py
+++ movie_review/movie_review/settings.py
@@ -215,6 +215,7 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "https://kikoky.shop",
+    "https://hack-2-fe.vercel.app",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
